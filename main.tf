90d9f1ac66a45c4dc99b0343f9cda28c3760a883
alexandermoroz01 <alexandermoroz01@gmail.com>
2025-05-09 23:46:09 +0300

    test deploy

---
+++ main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.11.4"
+}
+
+provider "aws" {
+  region  = "eu-north-1"
+}
+
+resource "aws_security_group" "allow_ssh_http_https" {
+  name        = "allow_ssh_http_https"
+  description = "Allow SSH, HTTP, and HTTPS traffic"
+  
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTPS
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ALL
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+user_data = file("setup.sh")
+
+  ami           = "ami-0c1ac8a41498c1a9c"
+  instance_type = "t3.micro"
+  key_name = "testDockerNginx"
+
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp3"
+  }
+
+  security_groups = [aws_security_group.allow_ssh_http_https.name]
+
+  tags = {
+    Name = "test-docker-nginx-terraform"
+  }
+}
---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+exec > /var/log/user-data.log 2>&1
+set -x
+
+apt update -y
+apt install -y docker.io git
+
+git clone https://github.com/alexandermoroz01/test-docker-nginx.git
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+cd test-docker-nginx
+docker build -t test-app .
+docker run -d -p 80:80 --name test-app --restart unless-stopped test-app
