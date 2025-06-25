2b11ecb8d7d83754df993c8bd41061080b844b08
alexandermoroz01 <alexandermoroz01@gmail.com>
2025-06-25 15:44:09 +0300

    ECS tf

--- ecs/main.tf
+++ ecs/main.tf
@@ -1,90 +1,118 @@
-provider "aws" {
-  region = "eu-north-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.11.4" # Мінімальна версія терраформа
 }
 
-# Отримуємо останній ECS-оптимізований AMI
+provider "aws" {
+  region  = "eu-north-1"
+}
+
+resource "aws_ecs_cluster" "test" {
+  name = "test-terraform"
+    setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_ecs_task_definition" "service" {
+  family = "service"
+  network_mode = "bridge"
+  requires_compatibilities = ["EC2"]
+  cpu = 512
+  memory = 512
+  execution_role_arn       = "arn:aws:iam::599076352459:role/ecsTaskExecutionRole"
+  task_role_arn            = "arn:aws:iam::599076352459:role/ecsTaskExecutionRole"
+  
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
+
+  container_definitions = jsonencode([
+    {
+      name = "first"
+      image = "599076352459.dkr.ecr.eu-north-1.amazonaws.com/test-app:latest"
+      essential = true
+      portMappings = [
+        {
+          name = "test-container-80-tcp"
+          containerPort = 80
+          hostPort      = 80
+          protocol = "tcp"
+          appProtocol = "http"
+        }
+      ]
+/*      
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/my-app"
+          awslogs-region        = "eu-north-1"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+*/
+    }
+  ])
+}
+
+resource "aws_ecs_service" "service" {
+  name = "test-service"
+  cluster = aws_ecs_cluster.test.id
+  task_definition = aws_ecs_task_definition.service.arn
+  desired_count = 1
+
+  launch_type = "EC2"
+}
+
 data "aws_ssm_parameter" "ecs_ami" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
 }
 
-# ECS кластер
-resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "my-ecs-cluster"
-}
-
-# Security Group для EC2
-resource "aws_security_group" "ecs_sg" {
-  name        = "ecs_sg"
-  description = "Allow HTTP"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Launch Template
-resource "aws_launch_template" "ecs_template" {
-  name_prefix   = "ecs-launch-template"
-  image_id      = data.aws_ssm_parameter.ecs_ami.value
+resource "aws_launch_template" "ecs" {
+  name_prefix = "test-terraform-"
+  image_id = data.aws_ssm_parameter.ecs_ami.value
   instance_type = "t3.micro"
-
-  vpc_security_group_ids = [aws_security_group.ecs_sg.id]
-
-  user_data = base64encode(file("user_data.sh"))
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name = "ECS Instance"
-    }
+  iam_instance_profile {
+    name = "ecsInstanceRole"
   }
+
+  user_data = base64encode(<<EOF
+  #!/bin/bash
+  echo "ECS_CLUSTER=${aws_ecs_cluster.test.name}" >> /etc/ecs/ecs.config
+  EOF
+  )
 }
 
-# Auto Scaling Group для EC2 під ECS
 resource "aws_autoscaling_group" "ecs_asg" {
-  desired_capacity     = 1
-  max_size             = 1
-  min_size             = 1
-  vpc_zone_identifier  = [aws_subnet.public_1.id]
+  name = "ecs-asg"
+  max_size = 1
+  min_size = 1
+  desired_capacity = 1
 
   launch_template {
-    id      = aws_launch_template.ecs_template.id
-    version = "$Latest"
+    id = aws_launch_template.ecs.id
+    version = aws_launch_template.ecs.latest_version
   }
 
+  vpc_zone_identifier = ["subnet-038357dbe22005793", "subnet-07d7a146391d065b5"]
   tag {
-    key                 = "AmazonECSCluster"
-    value               = aws_ecs_cluster.ecs_cluster.name
+    key                 = "Name"
+    value               = "${aws_launch_template.ecs.name_prefix}${aws_launch_template.ecs.id}"
     propagate_at_launch = true
   }
 }
 
-# ECS Task Definition (читає task-def.json)
-resource "aws_ecs_task_definition" "web_task" {
-  family                   = "web-task"
-  requires_compatibilities = ["EC2"]
-  network_mode             = "bridge"
-  cpu                      = "256"
-  memory                   = "512"
-  container_definitions    = file("task-def.json")
+/*
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+  name              = "/ecs/my-app"
+  retention_in_days = 7
 }
-
-# ECS Service (запускає контейнер на EC2 інстансі)
-resource "aws_ecs_service" "web_service" {
-  name            = "web-service"
-  cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.web_task.arn
-  desired_count   = 1
-  launch_type     = "EC2"
-}
+*/
--- ecs/user_data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "ECS_CLUSTER=my-ecs-cluster" >> /etc/ecs/ecs.config
-echo "ECS_BACKEND_HOST=ecs" >> /etc/ecs/ecs.config
